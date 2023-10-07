3a5d55066c95ea7913cbac34213617bfd2623424
Eduardo Ridao <manoguar@msn.com>
2023-10-07 07:45:48 +0200

    sdgfasdgsz

--- contracts/context/refundable.sol
+++ contracts/context/refundable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 import {RefundEscrow} from "@openzeppelin/contracts/utils/escrow/RefundEscrow.sol";
